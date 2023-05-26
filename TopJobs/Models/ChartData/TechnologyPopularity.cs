e54d50f01aef30682b77efd1addb728bb1402c07
Yrd-Q <94402417+Yrd-Q@users.noreply.github.com>
2023-05-26 11:39:36 +0300

    Add trends service

---
+++ TopJobs/Models/ChartData/TechnologyPopularity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopJobs.Models.ChartData
+{
+    public class TechnologyPopularity
+    {
+        public string TechnologyName { get; set; }
+        public int JobAdsNumber { get; set; }
+    }
+}
---
+++ TopJobs/Services/TrendsService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TopJobs.Data;
+using TopJobs.Models.ChartData;
+
+namespace TopJobs.Services
+{
+
+    public interface ITrendsService
+    {
+        List<TechnologyPopularity> GetTechnologyPopularities(int numOfTechnologies);
+        List<TechnologyPopularity> GetTechnologyTrend(DateTime startDate, DateTime endDate);
+    }
+    public class TrendsService : ITrendsService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMemoryCache _cache;
+        public TrendsService(IMemoryCache cache, ApplicationDbContext context)
+        {
+            _cache = cache;
+            _context = context;
+        }
+        public List<TechnologyPopularity> GetTechnologyPopularities(int numOfTechnologies)
+        {
+            //var technologyPopularities = _context.
+        }
+
+        public List<TechnologyPopularity> GetTechnologyTrend(DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
